4ec7b90ad76d3801fb0608541c4513fcd65c8a49
albertalegria <albertalegria@gmail.com>
2022-01-09 17:04:27 +0100

    debug false and accept all hosts

--- apm_project/settings.py
+++ apm_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*g1v=ha0n$jsqqg#0@fd1)^k1oc7w(0-*0_-mnau%^0n@-!g6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
